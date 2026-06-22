cd9d343f20c7e845c1072c2b32dd9fc38094b5fb
J-1000 <janredmann1@gmail.com>
2026-06-22 20:30:13 +0200

    raycast/translate: short-circuit on an empty target language

--- integrations/raycast/src/translate.tsx
+++ integrations/raycast/src/translate.tsx
@@ -1,7 +1,13 @@
+import { Detail } from "@raycast/api";
 import { ActionView } from "./action-view";
 
 export default function Command(props: { arguments: Arguments.Translate }) {
   const language = props.arguments.language.trim();
+  if (language.length === 0) {
+    return (
+      <Detail markdown="# Translate\n\nPlease provide a non-empty target language." />
+    );
+  }
   return (
     <ActionView
       action="translate"
